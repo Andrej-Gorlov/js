4a74771d00b43fcf6c241f8b7dd63a1c0b2c7e53
Ane <avgorlov889@gmail.com>
2022-12-13 20:12:37 +0300

    add proej

---
+++ JS/script.js
@@ -0,0 +1,71 @@
+"use strict"; //соврименный режим 
+
+/*let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+const obj = {
+    a: 50
+};
+obj.a = 30;
+console.log(obj.a);
+alert(5);
+[].push('a');*/
+/*let number = 4.6;
+console.log(-4 / 0);
+console.log("9" * 9);
+const persen = "Alex";
+const bool = true;
+//console.log(qwerty);
+let und;
+console.log(und);*/
+/*const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+let arr = [1,2,3,4,5,"sdf"];
+console.log(arr[5]);*/
+
+//alert("Hello");
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// // // // // const answe = +prompt("Вам есть 18?", "18");
+// // // // // console.log(answe + 5);
+
+/*const answers = [];
+
+answers[0] = prompt("Как Вас зовут?", "");
+answers[1] = prompt("Сколько Вам лет?", "");
+answers[2] = prompt("Какая у Вас фамилия?", "");*/
+
+//document.write(answers);
+/*console.log(typeof (answers));
+console.log(typeof (null));*/
+
+/*const category = "toys";
+console.log(`http://ya.ru/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Hello ${user}`)*/
+
+console.log('arr' + " - object");
+console.log(5 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr + "   " + --decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+
